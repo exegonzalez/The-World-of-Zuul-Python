f18f0a219df875f9540244bea845ed24fefd3102
Exequiel <exe.gonzalez14@hotmail.com>
2020-08-25 15:18:43 -0300

    add game

--- Parser.py
+++ Parser.py
@@ -1,4 +1,23 @@
-from Clase_CommandsWords import CommandsWords
+from CommandWords import CommandWords
+from Command import Command
+
 class Parser:
     def __init__(self):
         self.commands = CommandWords()
+
+    def getCommand(self):
+        word1 = None
+        word2 = None
+
+        inputLine = input("> ")
+        text = inputLine.split()
+        if (len(text)>0):
+            word1 = text[0]
+            if(len(text)>1):
+                word2 = text[1]
+
+        if(self.commands.isCommand(word1)):
+            return Command(word1,word2)
+        else:
+            return Command(None, word2)
+
